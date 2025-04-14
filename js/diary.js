3a259057572229be93ebcf5dd6c00f8609cbed9a
unknown <jys_troy@naver.com>
2025-04-14 16:20:02 +0900

    일정 등록 기능 구현 중

---
+++ js/diary.js
@@ -0,0 +1,31 @@
+const diary = {
+    date: null,
+    init() {
+        const searchParams = new URLSearchParams(location.search);
+        const date = searchParams.get("date");
+        /* try {
+            if (!date) {
+                throw new Error("날짜를 먼저 선택하세요.");
+            }
+
+            frmRegist.date.value = date;
+
+        } catch (err) {
+            alert(err.message);
+            location.href='index.html';
+        } */
+    },
+    add(item) {
+
+    },
+    edit(seq, item) {
+
+    },
+    remove(seq) {
+
+    }  
+}
+
+window.addEventListener("DOMContentLoaded", function() {
+    diary.init();
+});
