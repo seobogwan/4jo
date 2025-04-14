2f30664744a81479e07b86a4c09f00b4df3da6b2
seobogwan <tjqhrhks111>
2025-04-14 17:43:16 +0900

    diary_add.js 추가

--- js/diary.js
+++ js/diary.js
@@ -1,31 +1,3 @@
-const diary = {
-    date: null,
-    init() {
-        const searchParams = new URLSearchParams(location.search);
-        const date = searchParams.get("date");
-        /* try {
-            if (!date) {
-                throw new Error("날짜를 먼저 선택하세요.");
-            }
+window.addEventListener("DOMContentLoaded", function(){
 
-            frmRegist.date.value = date;
-
-        } catch (err) {
-            alert(err.message);
-            location.href='index.html';
-        } */
-    },
-    add(item) {
-
-    },
-    edit(seq, item) {
-
-    },
-    remove(seq) {
-
-    }  
-}
-
-window.addEventListener("DOMContentLoaded", function() {
-    diary.init();
 });
