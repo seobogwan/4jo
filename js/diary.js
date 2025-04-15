6408ad7334a03138055609eb3b6d44cf9c7bf425
unknown <jys_troy@naver.com>
2025-04-15 09:47:36 +0900

    변경사항 수정

--- js/diary.js
+++ js/diary.js
@@ -1,1 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.forms["frmRegist"];
 
+    form.addEventListener("submit", function (e) {
+        e.preventDefault(); 
+      
+        location.href = "index.html";
+    });
+});
